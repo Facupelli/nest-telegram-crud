52f7ef6936a334de02586cd79aa76ef488068498
Facundo Pellicer <facundopellicer4@gmail.com>
2023-07-07 20:39:06 -0300

    delete message service

--- src/message/message.service.ts
+++ src/message/message.service.ts
@@ -2,36 +2,36 @@ import { CollectionReference } from '@google-cloud/firestore';
 import { HttpService } from '@nestjs/axios';
 import { Injectable, Inject } from '@nestjs/common';
 import { MessageDocument } from './message.document';
-import { PostMessagetDto } from './dto';
-import {map, catchError, lastValueFrom} from "rxjs"
+import { DeleteMessagetDto, PostMessagetDto } from './dto';
+import { map, catchError, lastValueFrom } from 'rxjs';
 import { ForbiddenException } from '@nestjs/common';
 
 type PostResponse = {
-    ok: boolean,
-    result: {
-      message_id: number,
-      from: {
-        id: number,
-        is_bot: boolean,
-        first_name: string,
-        username: string 
-      },
-      chat: {
-        first_name: string, 
-        username: string, 
-        type: string, 
-      },
-      date: number,
-      text: string 
-    }
-}
+  ok: boolean;
+  result: {
+    message_id: number;
+    from: {
+      id: number;
+      is_bot: boolean;
+      first_name: string;
+      username: string;
+    };
+    chat: {
+      first_name: string;
+      username: string;
+      type: string;
+    };
+    date: number;
+    text: string;
+  };
+};
 
 @Injectable()
 export class MessageService {
   constructor(
     @Inject(MessageDocument.collectionName)
     private collection: CollectionReference<MessageDocument>,
-    private readonly httpService: HttpService
+    private readonly httpService: HttpService,
   ) {}
 
   getAllMessages() {
@@ -39,30 +39,63 @@ export class MessageService {
   }
 
   async postMessage(dto: PostMessagetDto) {
-    const request = this.httpService.post('https://api.telegram.org/bot6348267525:AAG4GXEa0E5yDdYXbNVgWwLLub9A_m1w7hY/sendMessage',
-    dto).pipe(
-      map((res) => res.data),
-    )
-    .pipe(
-      catchError(() => {
-        throw new ForbiddenException('API not available');
-      }),
-    );
+    const request = this.httpService
+      .post(
+        'https://api.telegram.org/bot6348267525:AAG4GXEa0E5yDdYXbNVgWwLLub9A_m1w7hY/sendMessage',
+        dto,
+      )
+      .pipe(map((res) => res.data))
+      .pipe(
+        catchError(() => {
+          throw new ForbiddenException('API not available');
+        }),
+      );
 
-    const response : PostResponse = await lastValueFrom(request)
+    const response: PostResponse = await lastValueFrom(request);
 
-    await this.collection.add(response.result)
-    
-    const todos = await this.collection.doc().get()
+    if (response.ok) {
+      await this.collection.add(response.result);
+    }
 
-    return {todos, response};
+    return true;
   }
 
   updateMessage() {
     return 'update message';
   }
 
-  async deleteMessage(dto) {
-   return "delete"
+  async deleteMessage(dto: DeleteMessagetDto) {
+    const request = this.httpService
+      .post(
+        'https://api.telegram.org/bot6348267525:AAG4GXEa0E5yDdYXbNVgWwLLub9A_m1w7hY/deleteMessage',
+        dto,
+      )
+      .pipe(map((res) => res.data))
+      .pipe(
+        catchError(() => {
+          throw new ForbiddenException('API not available');
+        }),
+      );
+
+    const response: { ok: boolean; result: boolean } = await lastValueFrom(
+      request,
+    );
+
+    if (response.result) {
+      const message = await this.collection.where(
+        'message_id',
+        '==',
+        dto.message_id,
+      );
+      message.get().then((snapshot) => {
+        snapshot.forEach(async (doc) => {
+          await doc.ref.delete();
+        });
+      });
+    }
+
+    console.log(response);
+
+    return true;
   }
 }
